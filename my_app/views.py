e17700e83391da189fe95a550dac4b3a3ad2b518
Durgaprasad-Dev <durgaprasad8883@gmail.com>
2020-05-12 17:05:06 +0530

    Before Scraping Work

--- my_app/views.py
+++ my_app/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def home(request):
     return render(request,'base.html')
 
+def  new_search(request):
+     # What we are typing in the search box we are retreiving
+     search=request.POST.get('search')
+     stuff_front_end={
+         'search':search
+     }
+     print(search)
+     #Context Dictionary
+     return render(request,'my_app/new_search.html',stuff_front_end)
+
